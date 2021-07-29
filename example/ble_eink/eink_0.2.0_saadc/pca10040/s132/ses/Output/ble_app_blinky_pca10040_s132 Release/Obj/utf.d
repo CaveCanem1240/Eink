6536291b5052ab9ea294b2984a7f4ca12fe5510a
Cave Canem <523048190@qq.com>
2021-07-29 15:12:45 +0800

    triggered by saadc

---
+++ example/ble_eink/eink_0.2.0_saadc/pca10040/s132/ses/Output/ble_app_blinky_pca10040_s132 Release/Obj/utf.d
@@ -0,0 +1,5 @@
+Output/ble_app_blinky_pca10040_s132\ Release/Obj/utf.o: \
+ C:\Users\52304\Desktop\nRF5_SDK_16.0.0_98a08e2\external\utf_converter\utf.c \
+ C:\Users\52304\Desktop\nRF5_SDK_16.0.0_98a08e2\external\utf_converter\utf.h \
+ F:/SEG/include/stddef.h F:/SEG/include/__crossworks.h \
+ F:/SEG/include/stdint.h
