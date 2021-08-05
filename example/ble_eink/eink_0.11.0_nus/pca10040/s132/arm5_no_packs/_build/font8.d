ce78107dcf21120b750ea039fdb561e2beaf7725
Cave Canem <523048190@qq.com>
2021-08-05 18:31:26 +0800

    import nordic uart service

---
+++ example/ble_eink/eink_0.11.0_nus/pca10040/s132/arm5_no_packs/_build/font8.d
@@ -0,0 +1,3 @@
+.\_build\font8.o: ..\..\..\Fonts\font8.c
+.\_build\font8.o: ..\..\..\Fonts\fonts.h
+.\_build\font8.o: F:\keil\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ example/ble_eink/eink_0.11.0_nus/pca10040/s132/arm5_no_packs/_build/ses_startup_nrf52840.d
@@ -0,0 +1,1 @@
+.\_build\ses_startup_nrf52840.o: ..\..\..\..\..\..\modules\nrfx\mdk\ses_startup_nrf52840.s
