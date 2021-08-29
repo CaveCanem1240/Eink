fbc70cba5730cb6425e63b0bb0e293295138c543
Cave Canem <523048190@qq.com>
2021-08-29 20:50:54 +0800

    map generator

---
+++ example/ble_eink/eink_4.0.1_map/Sokoban/Sokoban.h
@@ -0,0 +1,21 @@
+/***************************************************************************************/
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h> //memset()
+#include <math.h>
+
+#define UBYTE   uint8_t
+#define UWORD   uint16_t
+#define UDOUBLE uint32_t
+
+
+typedef struct {
+    int *ELEMENTS;
+    int Width;
+    int Height;
+} MAP;
+extern MAP Map;
+
+void GEN_MAP_PIC(int32_t chapter);
+
+
